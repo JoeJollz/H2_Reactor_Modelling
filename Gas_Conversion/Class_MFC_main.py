fc78d01118b6a9c39f5ff9999222a1d443fc91d6
JoeJollz <joseph.jolley@student.manchester.ac.uk>
2024-07-19 11:38:17 +0100

    basic MFC layout

--- Gas_Conversion/Class_MFC_main.py
+++ Gas_Conversion/Class_MFC_main.py
@@ -1,2 +1,28 @@
 "MFC Class conversion main" 
-testing
+
+import MFC_Converter
+
+
+class MassFlowController:
+    def __init__(self, mfc_number):
+        self.mfc_number = mfc_number
+        
+    def CO2_CO_reading(self, desired_mass_flow_lpm, guage_pressure):
+        
+        
+        return mass_flow, baudrate
+        
+if __name__ == "__main__":
+    
+    mfc = MassFlowController(1)
+    
+    desired_flow_rate_CO2 = 1 #lpm
+    guage_pressure = 1 #bar
+    
+    mass_flow, baudrate = mfc.CO2_CO_reading(desired_flow_rate_CO2,guage_pressure)
+    
+    print(f"Mass flow to set for MFC number 1: {mass_flow} ln/min, with baudrate {baudrate}")
+    
+    
+    
+        
