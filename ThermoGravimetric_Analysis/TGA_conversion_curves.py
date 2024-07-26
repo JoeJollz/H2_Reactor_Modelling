1d031d2a1b3e80d8a0497177b5045c965afc57e6
JoeJollz <joseph.jolley@student.manchester.ac.uk>
2024-07-26 23:06:06 +0100

    Oxidation and Reduction conversion curve extration complete. Needs tidying

--- ThermoGravimetric_Analysis/TGA_conversion_curves.py
+++ ThermoGravimetric_Analysis/TGA_conversion_curves.py
@@ -16,7 +16,7 @@ removed.
 max_peaks = 85.5
 index_start = 0 # no redox curves prior to this index.
 index_end = 89700//3 #  no redox curves beyond this point
-stoichometric_starting_mass = 84.635
+_starting_mass = 84.635#77.9499#84.635
 
 '''
 Step 2.
@@ -44,20 +44,34 @@ df_2 = pd.read_csv(
 
 sum_timer = 0
 air_start = []
+h2_start = []
+co2_start = []
 air_end = []
+h2_end = []
+co2_end = []
 for i in range(0, len(df_2)):
     if df_2.iloc[i, 1] == 1: # air gas injection
         air_start.append(sum_timer)
+    if df_2.iloc[i,1] ==2: # hydrogen gas injection
+        h2_start.append(sum_timer)
+    if df_2.iloc[i,1] == 4:
+        co2_start.append(sum_timer)
+    
     sum_timer += df_2.iloc[i,0]
     if df_2.iloc[i,1] ==1: #air gas injection ended
         air_end.append(sum_timer)
+    if df_2.iloc[i,1] ==2: # hydrogen gas injection ended
+        h2_end.append(sum_timer)
+    if df_2.iloc[i,1] == 4: # co2 gas injection ended
+        co2_end.append(sum_timer)
         
 def is_within_ranges(time, start_times, end_times):
     for start, end in zip(start_times, end_times):
+        #print(start, end)
         if start <= time <= end:
-            print('found air curve at t: ', time)
+            #print('found air at t: ', time)
             return True
-    
+    #print('bounyancy check accept at t: ', time)
     return False
 
 # Ensure all data is numeric and handle missing values if any
@@ -69,25 +83,116 @@ df = df.iloc[:int(len(df) / 2), :]
 divisor = df["t [s]"][1] - df["t [s]"][0]
 plt.plot(np.arange(0,len(df_)//2),df_.iloc[:int(len(df_)/2), int(divisor)])
 plt.show()
+fig1, ax1 = plt.subplots()
 
+cooldown = coolup =c = 0
 ## peak remover ## and conversion curve plotter. Here we identify and plot the reduction and oxidation curves. 
 for i in range(0,len(df)-1):
-    if (df['Value [mg]'][i]/(df['Value [mg]'][i-1])>1.008 and i > 400):  # so i is the location of the drastic gas change. Gas switch located! 
-        df['Value [mg]'][i+1]= df['Value [mg]'][i]=df['Value [mg]'][i-1]
-        df['Value [mg]'][i+2]= df['Value [mg]'][i]=df['Value [mg]'][i-1]
-        
+    #print(df['Value [mg]'][i]/df['Value [mg]'][i-1]/1.008>1)
+    if (df['Value [mg]'][i]/(df['Value [mg]'][i-1])>1.008 and 
+        i > 100 and 
+        cooldown == 0
+        ):  # so i is the location of the drastic gas change. Gas switch located! 
+    
+        df['Value [mg]'][i+1]= df['Value [mg]'][i]=df['Value [mg]'][i-2]
+        df['Value [mg]'][i+2]= df['Value [mg]'][i]=df['Value [mg]'][i-2]
+        df['Value [mg]'][i+3]= df['Value [mg]'][i]=df['Value [mg]'][i-2]
+        cooldown = 1
         #check if reduction curve, or oxidation curve!
-        
+        print( df['t [s]'][i], df['Value [mg]'][i], df['Value [mg]'][i+50])
         if (df['Value [mg]'][i] < df['Value [mg]'][i+5] and
             i>index_start and 
             i<index_end and 
-            not is_within_ranges(df['t [s]'][i+1], air_start, air_end)): # oxidation taking place
+            not is_within_ranges(df['t [s]'][i+1], air_start, air_end) and
+            not is_within_ranges(df['t [s]'][i+1], h2_start, h2_end)): # oxidation taking place
+            
             data_to_plot = df['Value [mg]'][i:(i+(2370//3))]
             time = np.array(df['t [s]'][i:i+(2370//3)])-df['t [s]'][i]
             t = df['t [s]'][i]
-            plt.plot(time, data_to_plot)
-            plt.title(f'Start time {t} seconds')
+            c +=1
+
+            data_to_plot = (data_to_plot/_starting_mass-1)*100+100
+            # #data_to_plot = (data_to_plot-_starting_mass)/_starting_mass*100+100
+            # #ax.plot(time, data_to_plot/_starting_mass*100, label=f'Curve {c}')
+            ax1.plot(time, data_to_plot, label=f'Curve {c}, time {t}')
+            
+            if c == 4: 
+                temp = df['Tr [°C]'][i]
+                ax1.set_title(f'Oxidation curves at {temp} °C')
+                ax1.set_xlabel('CO$_2$ exposure time (seconds)')
+                ax1.set_ylabel('Relative Mass Change (w.r.t 100%)')
+                ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+                ax1.set_ylim(94, 101.1)  # Set y-axis limits here
+                #ax.set_xlim(0,500)
+                fig1.tight_layout(rect=[0, 0, 0.75, 1])  # Adjust the right side of the plot
+                plt.show()
+                fig1, ax1 = plt.subplots()
+                
+                c = 0   
+        
+                
+        if (is_within_ranges(df['t [s]'][i+1], h2_start, h2_end)
+            ): # reduction taking place
+            df['Value [mg]'][i] = 2000
+            t = df['t [s]'][i]
+            print(df['Value [mg]'][i], f'time: {t}')
+            
+    cooldown -= 0.1
+    cooldown = max(cooldown, 0)
+        
+    # if df['Value [mg]'][i]>max_peaks:
+    #     df['Value [mg]'][i] = df['Value [mg]'][i+4]
+    #     df['Value [mg]'][i] = df['Value [mg]'][i+4]
+c=0
+cooldown = 0
+#fig2, ax2 = plt.subplots()
+for i in range(0,len(df)-1):
+    if df['Value [mg]'][i]>1500:
+        print('bingo')
+    
+    if (df['Value [mg]'][i]/(df['Value [mg]'][i-1])>1.01 and 
+        i >50
+        ):  # so i is the location of the drastic gas change. Gas switch located! 
+        t = df['t [s]'][i]
+        print(f'accssed, t : {t}')
+        df['Value [mg]'][i]= df['Value [mg]'][i-1]
+        cooldown = 1
+        
+        data_to_plot = df['Value [mg]'][i:(i+(2370//3))]
+        time = np.array(df['t [s]'][i:i+(2370//3)])-df['t [s]'][i]
+        t = df['t [s]'][i]
+        c +=1
+        data_to_plot = (data_to_plot/_starting_mass-1)*100+100
+        if i<1050:
+            save = data_to_plot
+            plt.plot(np.array(data_to_plot))
+            plt.title(f'{t}')
             plt.show()
+        data_to_plot = np.array(data_to_plot)
+        #data_to_plot = (data_to_plot-_starting_mass)/_starting_mass*100+100
+        #ax.plot(time, data_to_plot/_starting_mass*100, label=f'Curve {c}')
+        plt.plot(time, data_to_plot, label=f'Curve {c}, time {t}')
+        print('added to plot')
+
+        if c == 4: 
+            temp = df['Tr [°C]'][i]
+            plt.title(f'Reduction curves at {temp} °C')
+            plt.xlabel('H$_2$ exposure time (seconds)')
+            plt.ylabel('Relative Mass Change (w.r.t 100%)')
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            #ax.set_ylim(94, 101.1)  # Set y-axis limits here
+            #ax.set_xlim(0,500)
+            plt.tight_layout(rect=[0, 0, 0.75, 1])  # Adjust the right side of the plot
+            plt.show()
+            #fig2, ax2 = plt.subplots()
+            
+            c = 0
+                
+                
+                
+                
+    cooldown -= 0.1
+    cooldown = max(cooldown, 0)
         
         
         
@@ -96,7 +201,7 @@ for i in range(0,len(df)-1):
         df['Value [mg]'][i] = df['Value [mg]'][i+4]
         df['Value [mg]'][i] = df['Value [mg]'][i+4]
 
-    
+        
 
 
 #df['Value [mg]'][1]= df['Value [mg]'][2] =1
